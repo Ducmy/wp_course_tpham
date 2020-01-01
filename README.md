11658493860d6222ade9dfb72493669b74af649e
cnvs_my <mynguyen@Macintoshs-MacBook-Pro-5.local>
2020-01-01 14:40:14 +0700

    Frist set up

---
+++ README.md
@@ -0,0 +1,34 @@
+## Tạo Wordpress với Docker Compose
+
+
+#### Clone source from GitHub
+
+    $git clone https://github.com/Ducmy/wp_docker.git
+    $git checkout master
+    
+#### Khởi động docker-compose:
+
+    $docker-compose up -d
+
+#### Tắt docker-compose:
+
+    $docker-compose down
+
+### Sau khi Khởi động
+
+#### URL wordpress:
+http://localhost:8000
+ 
+
+#### URL phpadmin:
+http://localhost:8080
+
+#### Database User:
+    mysql user: root
+    mysql pass: root
+    wordpress database: wordpress_db
+#### Tạo user wordpress và đăng nhập với user đó:
+    ví dụ: user wordpress: mynguyen123
+            password wordpress: mynguyen123
+#### Lập trình themes wordpress 
+    -> ./htdocs/wp-content/themes/<Đặt tên theme>/style.css
---
+++ htdocs/wp-config.php
@@ -0,0 +1,96 @@
+<?php
+/**
+ * The base configuration for WordPress
+ *
+ * The wp-config.php creation script uses this file during the
+ * installation. You don't have to use the web site, you can
+ * copy this file to "wp-config.php" and fill in the values.
+ *
+ * This file contains the following configurations:
+ *
+ * * MySQL settings
+ * * Secret keys
+ * * Database table prefix
+ * * ABSPATH
+ *
+ * @link https://codex.wordpress.org/Editing_wp-config.php
+ *
+ * @package WordPress
+ */
+
+// ** MySQL settings - You can get this info from your web host ** //
+/** The name of the database for WordPress */
+define( 'DB_NAME', 'wordpress_db');
+
+/** MySQL database username */
+define( 'DB_USER', 'root');
+
+/** MySQL database password */
+define( 'DB_PASSWORD', 'root');
+
+/** MySQL hostname */
+define( 'DB_HOST', 'db:3306');
+
+/** Database Charset to use in creating database tables. */
+define( 'DB_CHARSET', 'utf8');
+
+/** The Database Collate type. Don't change this if in doubt. */
+define( 'DB_COLLATE', '');
+
+/**#@+
+ * Authentication Unique Keys and Salts.
+ *
+ * Change these to different unique phrases!
+ * You can generate these using the {@link https://api.wordpress.org/secret-key/1.1/salt/ WordPress.org secret-key service}
+ * You can change these at any point in time to invalidate all existing cookies. This will force all users to have to log in again.
+ *
+ * @since 2.6.0
+ */
+define( 'AUTH_KEY',         '2ad65f4512bd9ad229772aac3411ce1822eb3fc0');
+define( 'SECURE_AUTH_KEY',  '8534f94b539f0279cffe4189569d67d6f27bafb0');
+define( 'LOGGED_IN_KEY',    'dc4665d29c8e90a7dbea571555e568cb2475c57a');
+define( 'NONCE_KEY',        'e640d537d3371a0bd38fc95a76470b980f43ad14');
+define( 'AUTH_SALT',        'a140ee9ae40e531a95daebfcd4b4bf91d48c965b');
+define( 'SECURE_AUTH_SALT', 'e8ad7ed7a0b18830927f18308b2ab1826108763e');
+define( 'LOGGED_IN_SALT',   'f3b0e5ee1e9f0e4f2cfc48245b38e390a1fa454b');
+define( 'NONCE_SALT',       '1138d9c442396184d3d2f384b24de6e70dbdef07');
+
+/**#@-*/
+
+/**
+ * WordPress Database Table prefix.
+ *
+ * You can have multiple installations in one database if you give each
+ * a unique prefix. Only numbers, letters, and underscores please!
+ */
+$table_prefix = 'wp_';
+
+/**
+ * For developers: WordPress debugging mode.
+ *
+ * Change this to true to enable the display of notices during development.
+ * It is strongly recommended that plugin and theme developers use WP_DEBUG
+ * in their development environments.
+ *
+ * For information on other constants that can be used for debugging,
+ * visit the Codex.
+ *
+ * @link https://codex.wordpress.org/Debugging_in_WordPress
+ */
+define( 'WP_DEBUG', false );
+
+// If we're behind a proxy server and using HTTPS, we need to alert WordPress of that fact
+// see also http://codex.wordpress.org/Administration_Over_SSL#Using_a_Reverse_Proxy
+if (isset($_SERVER['HTTP_X_FORWARDED_PROTO']) && $_SERVER['HTTP_X_FORWARDED_PROTO'] === 'https') {
+	$_SERVER['HTTPS'] = 'on';
+}
+
+/* That's all, stop editing! Happy publishing. */
+
+/** Absolute path to the WordPress directory. */
+if ( ! defined( 'ABSPATH' ) ) {
+	define( 'ABSPATH', dirname( __FILE__ ) . '/' );
+}
+
+/** Sets up WordPress vars and included files. */
+require_once( ABSPATH . 'wp-settings.php' );
